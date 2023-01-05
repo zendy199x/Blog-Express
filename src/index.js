0da86ed552d4b0b46518365919555edea04b6291
Zendy <nguyenduc.051194@gmail.com>
2023-01-06 01:03:53 +0700

    Add template engine handlebars

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    // defaultLayout: "main",
+    // layoutsDir: path.join(__dirname, "resources/views/layouts"),
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
